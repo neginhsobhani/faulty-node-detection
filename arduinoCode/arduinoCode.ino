b868da1457bdd7a337926edf3e2c252e05c4b95f
neginhsobhani1 <neginhsobhani@gmail.com>
2022-11-25 21:17:53 +0330

    Arduino posting interval updated
    Arduino sends the average of the last 15 seconds temperatures sensed by sensors

--- arduinoCode/arduinoCode.ino
+++ arduinoCode/arduinoCode.ino
@@ -7,68 +7,110 @@
 #define sensorPin2 A2
 
 const unsigned long postingInterval = 15L * 1000L;
-
+long lastUpdateTime = 0;
+int intervalCounter = 0;
 //Initialise Arduino to NodeMCU (5=Rx & 6=Tx)
 SoftwareSerial nodemcu(5, 6);
 //SoftwareSerial espSerial(5, 6);
 
+// Temperature read from sensor each second
 float temp_sensor0;
 float temp_sensor1;
 float temp_sensor2;
 
-void setup(){
-  Serial.begin(115200);
-  
-  nodemcu.begin(115200);
-  delay(1000);
-  
+// Sum of temperatures read from sensor in a posting interval
+float sumSensor0;
+float sumSensor1;
+float sumSensor2;
+
+void setup() {
+  Serial.begin(9600);
+
+  nodemcu.begin(9600);
+  //  delay(1000);
+
   Serial.println("Program started");
 }
 void loop()
 {
-  StaticJsonBuffer<1000> jsonBuffer;
-  JsonObject& data = jsonBuffer.createObject();
-  
   //Obtain Tempreture from 3 sensors
   lm35_func();
-  
-  //Assign collected data to JSON Object
-  data["sensor0"] = temp_sensor0;
-  data["sensor1"] = temp_sensor1;
-  data["sensor2"] = temp_sensor2;
-//  data.printTo(Serial);
-  data.printTo(nodemcu);
-  jsonBuffer.clear();
+  intervalCounter ++; 
+  Serial.println(intervalCounter);
+  long now = millis() - lastUpdateTime;
+  Serial.println(millis());
+  Serial.println(lastUpdateTime);
+  Serial.println(postingInterval);
+  Serial.println(now);
+  if (millis() - lastUpdateTime >=  postingInterval) {
+    lastUpdateTime = millis();
+    Serial.print("reached post interval!");
+    Serial.println(millis());
+    StaticJsonBuffer<1000> jsonBuffer;
+    JsonObject& data = jsonBuffer.createObject();
 
-  delay(postingInterval);
+    //Assign collected data to JSON Object
+    data["sensor0"] = sumSensor0 / 15;
+    data["sensor1"] = sumSensor1 / 15;
+    data["sensor2"] = sumSensor2 / 15;
+
+    //  data.printTo(Serial);
+    data.printTo(nodemcu);
+    jsonBuffer.clear();
+
+    sumSensor0 = 0;
+    sumSensor1 = 0;
+    sumSensor2 = 0;
+
+//    delay(1000);
+  }
+  if (intervalCounter < 15){
+    delay(1000);
+    }
+  else {
+    // intervalCounter is 15
+    Serial.println("the last interval counter");
+    Serial.println(intervalCounter);
+    Serial.println("--------------------------");
+    intervalCounter = 0;
+    }
 }
 
-void lm35_func(){
+void lm35_func() {
   int reading0 = analogRead(sensorPin0);
   float voltage0 = reading0 * (5.0 / 1024.0);
   temp_sensor0 = voltage0 * 100;
+  sumSensor0 += temp_sensor0;
 
   int reading1 = analogRead(sensorPin1);
   float voltage1 = reading1 * (5.0 / 1024.0);
   temp_sensor1 = voltage1 * 100;
+  sumSensor1 += temp_sensor1;
 
   int reading2 = analogRead(sensorPin2);
   float voltage2 = reading2 * (5.0 / 1024.0);
   temp_sensor2 = voltage2 * 100;
+  sumSensor2 += temp_sensor2;
 
   // Print the tempreture in Celsius
   Serial.print("Temperature sensor 0: ");
   Serial.print(temp_sensor0);
+  Serial.print("Temperature Sum 0: ");
+  Serial.print(sumSensor0);
   Serial.print("\xC2\xB0"); // shows degree symbol
   Serial.println("C");
 
   Serial.print("Temperature sensor 1: ");
   Serial.print(temp_sensor1);
+  Serial.print("Temperature Sum 1: ");
+  Serial.print(sumSensor1);
   Serial.print("\xC2\xB0"); // shows degree symbol
   Serial.println("C");
 
   Serial.print("Temperature sensor 2: ");
   Serial.print(temp_sensor2);
+  Serial.print("Temperature Sum 2: ");
+  Serial.print(sumSensor2);
   Serial.print("\xC2\xB0"); // shows degree symbol
   Serial.println("C");
-  }
+}
--- espCode/espCode.ino
+++ espCode/espCode.ino
@@ -1,68 +1,38 @@
-#include <ThingSpeak.h>
-#include <ESP8266WiFi.h>
+#include <SoftwareSerial.h>
+#include <ArduinoJson.h>
 
-// Network parameters
-const char* ssid     = "HS";
-const char* password = "nhs780726";
-
-// ThingSpeak information
-char thingSpeakAddress[] = "api.thingspeak.com";
-unsigned long channelID = 1943777;
-//char* readAPIKey = "XXXXXXXXXXXXXXXX";
-char* writeAPIKey = "0D6EJXQGWY8KNG3T";
-const unsigned long postingInterval = 15L * 1000L;
-unsigned int dataFieldOne = 1;                            // Field to write temperature data
-unsigned int dataFieldTwo = 2;                       // Field to write temperature data
-unsigned int dataFieldThree = 3;                     // Field to write temperature data
-
-unsigned long lastConnectionTime = 0;
-long lastUpdateTime = 0; 
-WiFiClient client;
+// D6 = Rx & D5 = Tx
+SoftwareSerial nodemcu(D6, D5);
 
 void setup() {
-
-Serial.begin(115200);
-Serial.println("Start");
-connectWiFi();
-
+  // Initialize Serial port
+  Serial.begin(9600);
+  nodemcu.begin(9600);
+  while (!Serial) continue;
 }
+void loop() { // run over and over
+  StaticJsonBuffer<1000> jsonBuffer;
+  JsonObject& data = jsonBuffer.parseObject(nodemcu);
 
-void loop() {
-  
- // Update only if the posting time is exceeded
-    if (millis() - lastUpdateTime >=  postingInterval) {
-        
-        float fahrenheitTemperature, celsiusTemperature;
-        
-        lastUpdateTime = millis();
-        
-        celsiusTemperature = 237.15;
-        fahrenheitTemperature = celsiusTemperature * 9 / 5 + 32;
-        Serial.println("ADC =  " + String( celsiusTemperature )+ " Temp = "+String( fahrenheitTemperature ));
-        write2TSData( channelID , dataFieldOne , fahrenheitTemperature , dataFieldTwo , celsiusTemperature , dataFieldThree , millis() );      // Write the temperature in F, C, and time since starting.
-    }
-}
+  if (data == JsonObject::invalid()) {
+    //    Serial.println("Invalid Json Object");
+    jsonBuffer.clear();
+    return;
+  }
 
-void connectWiFi(){
-    WiFi.begin( ssid, password );
-    while (WiFi.status() != WL_CONNECTED) {
-      
-//        WiFi.begin( ssid, password );
-        delay(2500);
-        Serial.print(".");
-    }
-    
-    Serial.println( "Connected" );
-    ThingSpeak.begin( client );
-}
+  Serial.println("JSON Object Recieved");
+  Serial.println(millis());
+  Serial.print("Recieved Sensor0:  ");
+  float temp_sensor0 = data["sensor0"];
+  Serial.println(temp_sensor0);
 
-// Use this function if you want to write multiple fields simultaneously.
-int write2TSData( long TSChannel, unsigned int TSField1, float field1Data, unsigned int TSField2, long field2Data, unsigned int TSField3, long field3Data ){
+  Serial.print("Recieved Sensor1:  ");
+  float temp_sensor1 = data["sensor1"];
+  Serial.println(temp_sensor1);
 
-  ThingSpeak.setField( TSField1, field1Data );
-  ThingSpeak.setField( TSField2, field2Data );
-  ThingSpeak.setField( TSField3, field3Data );
-   
-  int writeSuccess = ThingSpeak.writeFields( TSChannel, writeAPIKey );
-  return writeSuccess;
+  Serial.print("Recieved Sensor2:  ");
+  float temp_sensor2 = data["sensor2"];
+  Serial.println(temp_sensor2);
+  Serial.println("-----------------------------------------");
+
 }
